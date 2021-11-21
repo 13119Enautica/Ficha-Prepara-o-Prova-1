07736accced0dd69e1c20bcaadc278709157c571
13119Enautica <13119@enautica.pt>
2021-11-21 21:02:37 +0000

    1 first time

--- src/Exercicio5.java
+++ src/Exercicio5.java
@@ -10,10 +10,10 @@ public class Exercicio5 {
 
         if(a>0){
             System.out.println("maior que 0");
-            if(a>100){
-                System.out.println("maior que 100");
-            }else if(a>500){
-                System.out.println("mairo que 500");
+            if(a<100){
+                System.out.println("menor que 100");
+            }else if(a<500){
+                System.out.println("menor que 500");
             }
         }else{
             System.out.println("menor que 0");
