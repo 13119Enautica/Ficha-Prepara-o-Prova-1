f26ce54b2308160305bb20adb312d583b12fed2e
13119Enautica <13119@enautica.pt>
2021-12-06 11:20:40 +0000

    1 first time

--- src/Exercicio7.java
+++ src/Exercicio7.java
@@ -1,6 +1,14 @@
 public class Exercicio7 {
-
     public static void main(String[] args) {
-        int trumas, cruso, datainicio, datafinal, Nomeprofessor, Telefoneprofessor;
+        int i=0;
+
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+        while(i< cars.length) {
+            System.out.println(cars[i]);
+            i ++;
+        }
+
+
     }
 }
