025de3064142ffa1e7fa4fa0fa18bbc5d2865ae1
Duarte <13119@enautica.pt>
2022-01-09 21:07:32 +0000

    0

---
+++ src/Exercicio8.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Exercicio8 {
+    public static void main(String[] args) {
+
+        int i;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Teste: ");
+        i = input.nextInt();
+
+
+        System.out.println("Teste: "+i);
+    }
+}
